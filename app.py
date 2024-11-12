8b780e8338721783caa6130633e12f5bad8e63b8
GemsLoveNLP <6438169421@student.chula.ac.th>
2024-11-12 23:11:55 +0700

    add: app.py comment

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import Flask, render_template, jsonify
 from modules.publisher import Publisher
 
+# create a Flask application
 app = Flask(__name__)
 
+# connect the MQTT publisher
 mqtt_publisher = Publisher()
 mqtt_publisher.connect()
 
+# render the UI in index.html
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# publish the OPEN command to the subscriber
 @app.route('/open')
 def open_button():
     mqtt_publisher.publish("OPEN")
     return jsonify(status="success")
 
+# publish the CLOSE command to the subscriber
 @app.route('/close')
 def close_button():
     mqtt_publisher.publish("CLOSE")
