a840946106b3d72fe3ab9c95f2042df3310d9e59
GemsLoveNLP <6438169421@student.chula.ac.th>
2024-11-12 18:19:38 +0700

    add: comment maannhai.py

--- modules/maannhai.py
+++ modules/maannhai.py
@@ -2,19 +2,25 @@ import time
 from gpiozero import LED, Button, DigitalInputDevice
 from utils.tts import speak
 
+# initialize a class for the control over the hardware
 class MaanNhai:
     def __init__(self, init_status="close"):
         # Assign pins
+
+        # LED
         self.red = LED(17)
         self.green = LED(27)
         self.blue = LED(22)
 
+        # push button
         self.button1 = Button(24, pull_up=True)
         self.button2 = Button(23, pull_up=True)
 
+        # limit switches
         self.lmPulley = DigitalInputDevice(12, pull_up=True, bounce_time = 0.1)
         self.lmMotor = DigitalInputDevice(6, pull_up=True, bounce_time = 0.1)
 
+        # stepper motor driver
         self.pul = LED(16)
         self.dir = LED(20)
         self.ena = LED(21)
@@ -56,6 +62,8 @@ class MaanNhai:
         self.led("111")
 
     # Motor control methods
+
+    # move the clicker piece to the motor side
     def moveToMotor(self):
         self.dir.off()
         time.sleep(0.0001)
@@ -65,6 +73,7 @@ class MaanNhai:
             self.pul.off()
             time.sleep(0.001 / self.speed)
 
+    # move the clicker piece to the pulley side
     def moveToPulley(self):
         self.dir.on()
         time.sleep(0.0001)
@@ -74,6 +83,7 @@ class MaanNhai:
             self.pul.off()
             time.sleep(0.001 / self.speed)
 
+    # stop the motor and move the clicker away from the limit switch (motor side)
     def stopMotor(self):
         self.ledRed()
         time.sleep(0.01)
@@ -82,6 +92,7 @@ class MaanNhai:
         time.sleep(0.5)
         self.ledOff()
 
+    # stop the motor and move the clicker away from the limit switch (pulley side)
     def stopPulley(self):
         self.ledRed()
         time.sleep(0.01)
@@ -91,6 +102,9 @@ class MaanNhai:
         self.ledOff()
 
     # Movement control methods
+
+    # move the clicker until it hits the motor side limit switch
+    # LED code is purple
     def moveUntilMotor(self):
         self.ledPurple()
         while self.lmMotor.is_active:
@@ -98,6 +112,8 @@ class MaanNhai:
         self.stopMotor()
         self.ledOff()
 
+    # move the clicker until it hits the pulley side limit switch
+    # LED code is blue
     def moveUntilPulley(self):
         self.ledBlue()
         while self.lmPulley.is_active:
@@ -105,6 +121,7 @@ class MaanNhai:
         self.stopPulley()
         self.ledOff()
 
+    # move to predifined home position
     def moveHome(self):
         self.ledWhite()
         while self.lmPulley.is_active:
@@ -126,24 +143,22 @@ class MaanNhai:
         else:
             self.handle_buttons()
 
+    # open the curtain
     def open_curtain(self):
         if self.status == "close":
             print("[DEVICE] Openning")
             self.ledCyan()
-            # self.moveToMotor()
-            # time.sleep(0.005)
             self.moveUntilMotor()
             self.status = "open"
             # speak("open")
             print("[DEVICE] Curtains are OPENED")
             self.ledGreen()
 
+    # close the curtain
     def close_curtain(self):
         if self.status == "open":
             print("[DEVICE] Closing")
             self.ledYellow()
-            # self.moveToPulley()
-            # time.sleep(0.005)
             self.moveUntilPulley()
             self.status = "close"
             # speak("close")
@@ -151,54 +166,69 @@ class MaanNhai:
             self.ledGreen()
 
 
+    # handle the push button operations
     def handle_buttons(self, queue):
         """
         Handles button presses to control the curtains.
         """
+
+        # wait for signal
         while True:
+
+            # default LED to green
             self.ledGreen()
 
+            # if the pulley side limit switch is hit
             if not self.lmPulley.is_active:
                 print("lmPulley active")
                 self.stopPulley()
                 self.status = "close"
                 # speak("close")
 
+            # if the motor side limit switch is hit
             if not self.lmMotor.is_active:
                 print("lmMotor active")
                 self.stopMotor()
                 self.status = "open"
                 # speak("open")
 
+            # if push button 1 is pressed
             if self.button1.is_pressed:
                 print("Button1 pressed")
+                # if closed, then move slightly to open, LED is cyan
                 if self.status == "close":
                     self.ledCyan()
                     self.moveToMotor()
                     time.sleep(0.005)
+                # if opend, then move slightly to close, LED is yellow
                 else:
                     self.ledYellow()
                     self.moveToPulley()
                     time.sleep(0.005)
 
+            # if push button 2 is pressed
             if self.button2.is_pressed:
                 print("Button2 pressed")
+                # if button 1 is also pressed, turns off the system
                 if self.button1.is_pressed:
                     self.moveHome()
                     print("Curtain Turning off")
                     # speak("Curtain turning off")
                     return
+                # if closed, then move until open
                 if self.status == "close":
                     self.moveUntilMotor()
                     time.sleep(0.01)
                     self.status = "open"
                     # speak("open")
+                # if opened, then move until close
                 else:
                     self.moveUntilPulley()
                     time.sleep(0.01)
                     self.status = "close"
                     # speak("close")
 
+            # sleep to better performance
             time.sleep(0.1)
 
 
